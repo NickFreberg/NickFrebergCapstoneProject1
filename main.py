dbe4ee1c18f3515a4beefe39b55318a3b97bb5cb
Nick Freberg <nfreberg@student.bridgew.edu>
2020-02-03 21:20:36 -0500

    working on parsing json information. Testing? Bleh...

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 import requests
 
-json_url = "https://jobs.github.com/positions.json?page=5&description=python&location=new+york"
-
+json_url = "https://jobs.github.com/positions.json?page=1&description=python&location=new+york"
+f = open("jsonderulo.txt",'w')
 response = requests.get(json_url)
 
 if response.status_code == 200:
     print("Successfully obtained json data")
-    #once json data is obtained, save it to a file
+    print("'\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\'")
 
+    json = response.json()
+    print(json[1])
+    for i in range(0, len(json)):
+        print(dict.get(json[i]))
+        print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    # once json data is obtained, save it to a file
 
 
 elif response.status_code == 404:
