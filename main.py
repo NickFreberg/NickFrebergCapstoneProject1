8abbfe2d12ce39f14b524d6e892ec6c6cbf8417c
Nick Freberg <nfreberg@student.bridgew.edu>
2020-02-03 22:31:47 -0500

    working code. onto tests

--- main.py
+++ main.py
@@ -1,20 +1,33 @@
 import requests
 
-json_url = "https://jobs.github.com/positions.json?page=1&description=python&location=new+york"
+json_url = "https://jobs.github.com/positions.json?page="
+# test "https://jobs.github.com/positions.json?page=1&description=python&location=new+york"
 f = open("jsonderulo.txt",'w')
-response = requests.get(json_url)
+total_listings = 0
+for i in range(0, 5):
+    pageurl = json_url + str(i + 1)
+    pgres = requests.get(pageurl)
+    if pgres.status_code == 200:
+        print("Successfully obtained json data")
+        print("'\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\'")
+        page_json = pgres.json()
+        total_listings = total_listings + int((len(page_json)))
+        print(page_json[1])
+        for j in range(0, len(page_json)):
+            print(dict.get(page_json[i], 'id'))
+            print(dict.get(page_json[i], 'type'))
+            print(dict.get(page_json[i], 'url'))
+            print(dict.get(page_json[i], 'created_at'))
+            print(dict.get(page_json[i], 'company'))
+            print(dict.get(page_json[i], 'company_url'))
+            print(dict.get(page_json[i], 'location'))
+            print(dict.get(page_json[i], 'title'))
+            print(dict.get(page_json[i], 'description'))
+            print(dict.get(page_json[i], 'how_to_apply'))
+            print(dict.get(page_json[i], 'company_logo'))
+            print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    print("Total listings: " + str(total_listings))
 
-if response.status_code == 200:
-    print("Successfully obtained json data")
-    print("'\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\'")
-
-    json = response.json()
-    print(json[1])
-    for i in range(0, len(json)):
-        print(dict.get(json[i]))
-        print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
     # once json data is obtained, save it to a file
 
 
-elif response.status_code == 404:
-    print("json data not found")
