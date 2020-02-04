5eb9a5a3ee0e5777a02b93e3445d8b8775f3dd8b
Nick Freberg <nfreberg@student.bridgew.edu>
2020-02-03 23:50:34 -0500

    working code! Tests? errr.... yeah about that :))

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
-# Nick Freberg
-# COMP 490
-# Project 1
-# Sprint 1
+Nick Freberg
 
-# What's Missing: everything
-# Requirements
-# pip install requests
+COMP 490
+
+Project 1
+Sprint 1
+
+What's Missing: 
+1. Ran into a problem with writing the description of each posting. Would cause an error when running the program
+2. Testing
--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import requests
 
+# def pagination(json_url,curr_page):
+
+
 json_url = "https://jobs.github.com/positions.json?page="
 # test "https://jobs.github.com/positions.json?page=1&description=python&location=new+york"
 f = open("jsonderulo.txt",'w')
@@ -8,26 +11,20 @@ for i in range(0, 5):
     pageurl = json_url + str(i + 1)
     pgres = requests.get(pageurl)
     if pgres.status_code == 200:
-        print("Successfully obtained json data")
-        print("'\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\'")
         page_json = pgres.json()
         total_listings = total_listings + int((len(page_json)))
-        print(page_json[1])
         for j in range(0, len(page_json)):
-            print(dict.get(page_json[i], 'id'))
-            print(dict.get(page_json[i], 'type'))
-            print(dict.get(page_json[i], 'url'))
-            print(dict.get(page_json[i], 'created_at'))
-            print(dict.get(page_json[i], 'company'))
-            print(dict.get(page_json[i], 'company_url'))
-            print(dict.get(page_json[i], 'location'))
-            print(dict.get(page_json[i], 'title'))
-            print(dict.get(page_json[i], 'description'))
-            print(dict.get(page_json[i], 'how_to_apply'))
-            print(dict.get(page_json[i], 'company_logo'))
-            print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-    print("Total listings: " + str(total_listings))
-
-    # once json data is obtained, save it to a file
-
-
+            f.write("~~~~~~~~~~~~~~~~~~~START OF POSTING~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
+            f.write(dict.get(page_json[i], 'id') + "\n")
+            f.write(dict.get(page_json[i], 'type') + "\n")
+            f.write(dict.get(page_json[i], 'url') + "\n")
+            f.write(dict.get(page_json[i], 'created_at') + "\n")
+            f.write(dict.get(page_json[i], 'company') + "\n")
+            f.write(dict.get(page_json[i], 'company_url') + "\n")
+            f.write(dict.get(page_json[i], 'location') + "\n")
+            f.write(dict.get(page_json[i], 'title') + "\n")
+            # f.write(dict.get(page_json[i], 'description') + "\n")
+            f.write(dict.get(page_json[i], 'how_to_apply') + "\n")
+            f.write(dict.get(page_json[i], 'company_logo') + "\n")
+            f.write("~~~~~~~~~~~~~~~~~~~END OF POSTING~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    print("Finished reading a page! Total listings: " + str(total_listings))
